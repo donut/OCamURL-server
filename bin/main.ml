e663b1847f226a0536e1e9765092ac04bd9635ba
Donovan Mueller <zotobi@gmail.com>
2017-11-14 11:49:27 -0700

    Add enableAlias endpoint.

--- bin/main.ml
+++ bin/main.ml
@@ -13,6 +13,7 @@ let schema db_conn = Gql.Schema.(Schema.(schema [
   ~mutations:[
     Add_alias_mut.field db_conn;
     Disable_alias_mut.field db_conn;
+    Enable_alias_mut.field db_conn;
     Generate_alias_mut.field db_conn;
   ]
 ))
