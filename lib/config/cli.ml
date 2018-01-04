5e5795ec9025aeabafa81678b7c5e4c31fac930b
Donovan Mueller <zotobi@gmail.com>
2018-01-04 15:02:24 -0700

    Update to latest Core.Command API.

--- lib/config/cli.ml
+++ lib/config/cli.ml
@@ -1,10 +1,10 @@
 
 open Core
 
-
 let start_service_command callback = Command.(
-	basic
+	basic_spec
 		~summary:"Start an OCamURL server."
+		~readme:(fun _ -> "")
 		Spec.(empty
 			+> anon ("service_name" %: string) 
 			+> anon ("config_path" %: file))
